974ac37debf2069d30bca7178210f7e95947f660
María <120553496+intergalacticmar@users.noreply.github.com>
2024-02-09 09:34:14 +0100

    guardado 2

---
+++ Assets/Scripts/PoolManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager : MonoBehaviour
+{
+    public static PoolManager Instance;
+
+    [SerializeField] string parentName;
+    [SerializeField] GameObject prefab;
+    [SerializeField] int poolSize;
+    [SerializeField] List<GameObject> pooledObjects;
+
+    void Awake()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject parent = new GameObject(parentName);
+        
+        GameObject obj;
+
+        for (int i = 0; i < length; i++)
+        {
+            obj = Instantiate(prefab);
+            obj.transform.SetParent(parent.transform);
+            obj.SetActive(false);
+            pooledObjects.Add(obj);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
